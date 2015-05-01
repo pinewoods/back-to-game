7017bd475c83ed9e0897f4d62507f03fc70476d2
ismaelrodrigues <rodrigues.ismael@gmail.com>
2015-04-30 21:35:58 -0300

    adding the second Activity

--- educare/app/src/main/java/br/com/educare/Home.java
+++ educare/app/src/main/java/br/com/educare/Home.java
@@ -1,9 +1,11 @@
 package br.com.educare;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class Home extends ActionBarActivity {
@@ -36,4 +38,11 @@ public class Home extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    /** Called when the user clicks the openContent button */
+    public void openContent(View view) {
+        Intent intent = new Intent(getApplicationContext(),ContentActivity.class);
+        startActivity(intent);
+
+    }
+
 }
