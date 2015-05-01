b3e9c44d975a6ef56a5fa7a82bfae006b47fcf9a
ismaelrodrigues <rodrigues.ismael@gmail.com>
2015-05-01 12:49:57 -0300

    Writing Content Activity

--- educare/app/src/main/java/br/com/educare/ClassroomActivity.java
+++ educare/app/src/main/java/br/com/educare/ClassroomActivity.java
@@ -1,26 +1,24 @@
 package br.com.educare;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class Home extends ActionBarActivity {
+public class ClassroomActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_classroom);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_classroom, menu);
         return true;
     }
 
@@ -38,21 +36,4 @@ public class Home extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    /** Called when the user clicks the openContent button */
-    public void openContent(View view) {
-        Intent intent = new Intent(getApplicationContext(),ContentActivity.class);
-        startActivity(intent);
-
-    }
-    public void openClassroom(View view) {
-        Intent intent = new Intent(getApplicationContext(),ClassroomActivity.class);
-        startActivity(intent);
-
-    }
-    public void openJournal(View view) {
-        Intent intent = new Intent(getApplicationContext(),JournalActivity.class);
-        startActivity(intent);
-
-    }
-
 }
